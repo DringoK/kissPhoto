ff2653562e365294cfc78861c328338f9cf4c7c3
ikr <ingo.kreuz@gmx.net>
2023-01-29 16:12:13 +0100

    new: - move files also while inline edit mode - improve keeping cursor position while in edit mode

--- src/dringo/kissPhoto/KissPhoto.java
+++ src/dringo/kissPhoto/KissPhoto.java
@@ -91,7 +91,7 @@ import java.util.ResourceBundle;
  */
 public class KissPhoto extends Application {
   //please check Log.debugLevel in main()
-  public static final String KISS_PHOTO_VERSION = "0.23.0129"; // <------------------------------------------------------------------------------
+  public static final String KISS_PHOTO_VERSION = "0.23.0129";  // <------------------------------------------------------------------------------
   public static final String KISS_PHOTO = "kissPhoto ";
   public static ResourceBundle language = null;
 
